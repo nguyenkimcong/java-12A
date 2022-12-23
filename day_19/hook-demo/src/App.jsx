af2328f7f44197bdb0f571a384167abe9a13a2a4
buihien0109 <buihien01091997@gmail.com>
2022-12-23 21:24:07 +0700

    update

---
+++ day_19/hook-demo/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Post from "./components/post/Post";
+
+function App() {
+    return (
+        <>
+            <Post />
+        </>
+    );
+}
+
+export default App;
