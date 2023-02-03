a0cd354b64eabbfbb93a4d576771d8f91bb524c8
buihien0109 <buihien01091997@gmail.com>
2023-02-03 21:17:27 +0700

    update

--- day_26/example-redux/src/App.jsx
+++ day_26/example-redux/src/App.jsx
@@ -1,9 +1,26 @@
+import { Link, Route, Routes } from "react-router-dom";
+import Counter from "./components/counter/Counter";
 import TodoList from "./components/todolist/TodoList";
+import NotFound from "./components/not-found/NotFound";
 
 function App() {
     return (
         <>
-            <TodoList />
+            <ul>
+                <li>
+                    <Link to={"/counter"}>Counter App</Link>
+                </li>
+                <li>
+                    <Link to={"/todolist"}>Todo App</Link>
+                </li>
+            </ul>
+
+            <Routes>
+                <Route path="/" element={<Counter />} />
+                <Route path="/counter" element={<Counter />} />
+                <Route path="/todolist" element={<TodoList />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
         </>
     );
 }
