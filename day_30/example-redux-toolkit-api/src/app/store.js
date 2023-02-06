568bf40168aad0b8d3f18dccd5d310887b03944e
buihien0109 <buihien01091997@gmail.com>
2023-02-06 20:39:19 +0700

    update

---
+++ day_30/example-redux-toolkit-api/src/app/store.js
@@ -0,0 +1,11 @@
+
+import { configureStore } from "@reduxjs/toolkit";
+import todosReducer from "./slices/todos.slice";
+
+const store = configureStore({
+    reducer: {
+        todos: todosReducer
+    }
+})
+
+export default store;
