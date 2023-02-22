77db9bd867c72b74dee15a8cacf2d6c055c43cdd
buihien0109 <buihien01091997@gmail.com>
2023-02-22 18:39:07 +0700

    update

--- day_32/security-basic/src/test/java/com/example/securitybasic/SecurityBasicApplicationTests.java
+++ day_32/security-basic/src/test/java/com/example/securitybasic/SecurityBasicApplicationTests.java
@@ -8,7 +8,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @SpringBootTest
 class SecurityBasicApplicationTests {
 
-
     @Autowired
     private PasswordEncoder passwordEncoder;
 
